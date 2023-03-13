45ca1a6e89e0e70c06a0dbc563016bb4a8c09965
Apodlubny <apodlubny@gmail.com>
2023-03-13 22:46:41 +0200

    structure

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-function App() {
-  return <div>Car application</div>;
-}
-
-export default App;
---
+++ src/components/screens/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Cars application</h1>
+      <div>
+        <div>
+          <h2>Car1</h2>
+          <p>100000$</p>
+          <button>Read more</button>
+        </div>
+      </div>
+    </div>
+  );
+};
