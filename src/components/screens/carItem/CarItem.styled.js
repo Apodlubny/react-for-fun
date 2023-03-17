17970588d7591db728e3bb9635b4c74ffe417087
Apodlubny <apodlubny@gmail.com>
2023-03-17 21:19:41 +0200

    Update CarItem.styled.js

--- src/components/screens/carItem/CarItem.styled.js
+++ src/components/screens/carItem/CarItem.styled.js
@@ -2,11 +2,8 @@ import styled from "@emotion/styled";
 
 export const Item = styled.div`
   border: 1px solid white;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
+
   width: 100%;
-  height: 280px;
 `;
 
 export const Image = styled.div`
@@ -14,7 +11,6 @@ export const Image = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   width: 100%;
-  height: 280px;
 `;
 
 export const InfoBox = styled.div`
