c321aec934d3f955c49b95113f8675bbb5d9a0e0
Apodlubny <apodlubny@gmail.com>
2023-03-17 21:14:45 +0200

    iimages

--- src/components/screens/carItem/CarItem.jsx
+++ src/components/screens/carItem/CarItem.jsx
@@ -4,10 +4,13 @@ import { Item, Image, InfoBox } from "./CarItem.styled";
 export const CarItem = ({ car: { name, price, image } }) => {
   return (
     <Item>
-      <Image image={image} />
+      <Image>
+        <img src={image}></img>
+      </Image>
       <InfoBox>
         <h2>{name}</h2>
         <p>{price}$</p>
+
         <button>Read more</button>
       </InfoBox>
     </Item>
--- src/components/screens/carItem/CarItem.styled.js
+++ src/components/screens/carItem/CarItem.styled.js
@@ -1,12 +1,15 @@
 import styled from "@emotion/styled";
-import BgCar1 from "/img/BgCar1.jpg";
 
 export const Item = styled.div`
   border: 1px solid white;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 280px;
 `;
 
 export const Image = styled.div`
-  background-image: url(${BgCar1});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
