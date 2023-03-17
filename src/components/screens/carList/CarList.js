700af5da2fae8cdb989e19d81c161d90cffd3bef
Apodlubny <apodlubny@gmail.com>
2023-03-17 20:58:37 +0200

    carList+carItem

--- src/components/screens/carList/CarList.js
+++ src/components/screens/carList/CarList.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { List } from "./CarList.styled";
+import { CarItem } from "../carItem/CarItem";
 
-export const CarList = () => {
-  return <List>CarList</List>;
+export const CarList = ({ cars }) => {
+  return (
+    <List>
+      {cars.map((car, id) => (
+        <li key={id}>
+          <CarItem car={car} />
+        </li>
+      ))}
+    </List>
+  );
 };
--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Main } from "./Home.styled";
-// import cars from "./cars.data.json";
+import { CarList } from "../carList/CarList";
+import cars from "./cars.data.json";
 
 export const Home = () => {
   return (
     <Main>
       <h1>Cars application</h1>
-      <div></div>
+      <div>
+        <CarList cars={cars} />
+      </div>
     </Main>
   );
 };
