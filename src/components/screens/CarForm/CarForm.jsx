e58022fc1719de7cb04777af10b213553f5ed9cc
Apodlubny <apodlubny@gmail.com>
2023-03-18 20:32:03 +0200

    form

---
+++ src/components/screens/CarForm/CarForm.jsx
@@ -0,0 +1,9 @@
+import { Form } from "./CarForm.styled";
+
+export const CarForm = () => {
+  return (
+    <Form>
+      <input type="text" placeholder="Name" />
+    </Form>
+  );
+};
--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Main } from "./Home.styled";
 import { CarList } from "../carList/CarList";
 import cars from "./cars.data.json";
+import { CarForm } from "../CarForm/CarForm";
 
 export const Home = () => {
   return (
     <Main>
       <h1>Cars application</h1>
+      <CarForm />
       <div>
         <CarList cars={cars} />
       </div>
